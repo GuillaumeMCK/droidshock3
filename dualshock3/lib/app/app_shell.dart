af62a78a56086d2913561a8aad3a551f16cadaf8
GuillaumeMCK <guillaume.mucwicki@gmail.com>
2026-03-26 16:55:41 +0100

    refactor(ui): configure system UI mode and device orientation in AppShellPage
    - Convert `AppShellPage` from `StatelessWidget` to `HookWidget`
    - Set system UI mode to `immersiveSticky` and restrict orientation to `portraitUp` using `useEffect` and `useAppLifecycleState`

--- dualshock3/lib/app/app_shell.dart
+++ dualshock3/lib/app/app_shell.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 import '/common/common.dart';
@@ -6,11 +7,18 @@ import '/common/common.dart';
 typedef AppConstraints = BoxConstraints;
 
 @RoutePage()
-class AppShellPage extends StatelessWidget {
+class AppShellPage extends HookWidget {
   const AppShellPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final state = useAppLifecycleState();
+    useEffect(() {
+      SystemChrome.setEnabledSystemUIMode(.immersiveSticky);
+      SystemChrome.setPreferredOrientations([.portraitUp]);
+      return null;
+    }, [state]);
+
     return LayoutBuilder(
       builder: (context, constraints) => Provider<AppConstraints>.value(
         value: constraints,
